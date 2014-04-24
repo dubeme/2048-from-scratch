ac21bb1b81e14e4e9bf7b22d3d843678c27c817e
Dubem Enyekwe <dumdumonline@yahoo.com>
2014-04-24 03:36:52 -0700

    Fixed some bugs in the game logic.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var gameBoard = [
-	[2,2,4,4],
-	[0,0,4,4],
-	[2,0,4,4],
-	[4,0,4,2]
+[2,2,4,4],
+[0,0,4,4],
+[2,0,4,4],
+[4,2,4,2]
 ];
 var DIR_LEFT = 0;
 var DIR_RIGHT = 1;
@@ -17,10 +17,10 @@ function move(dir) {
 	var line;
 	var move = false;
 
-	for (var index = 0; index < 4; index++ {
+	for (var index = 0; index < 4; index++) {
 		line = getLine(dir,index);
-		move = solveLine(line);
-		setRow(dir, index, line);
+		line = solveLine(line);
+		setLine(dir, index, line);
 	}
 }
 
@@ -31,6 +31,30 @@ function getLine(dir, index) {
 		return getCol(dir, index);
 
 	return null;
+}
+
+function setLine(dir, index, temp) {
+	if (dir === DIR_LEFT) {
+		gameBoard[index][0] = temp[3];
+		gameBoard[index][1] = temp[2];
+		gameBoard[index][2] = temp[1];
+		gameBoard[index][3] = temp[0];
+	} else if (dir === DIR_RIGHT) {
+		gameBoard[index][0] = temp[0];
+		gameBoard[index][1] = temp[1];
+		gameBoard[index][2] = temp[2];
+		gameBoard[index][3] = temp[3];
+	} else if (dir === DIR_UP) {
+		gameBoard[0][index] = temp[3];
+		gameBoard[1][index] = temp[2];
+		gameBoard[2][index] = temp[1];
+		gameBoard[3][index] = temp[0];
+	} else if (dir === DIR_DOWN) {
+		gameBoard[0][index] = temp[0];
+		gameBoard[1][index] = temp[1];
+		gameBoard[2][index] = temp[2];
+		gameBoard[3][index] = temp[3];
+	}
 }
 
 function getRow(dir, row) {
@@ -67,7 +91,7 @@ function getCol(dir, col) {
 				index--;
 			};
 		};
-	} else if (dir === DIR_RIGHT) {
+	} else if (dir === DIR_DOWN) {
 		for (var row = 3; row >= 0; row--) {
 			if (gameBoard[row][col] !== 0) {
 				temp[index] = gameBoard[row][col];
@@ -77,20 +101,6 @@ function getCol(dir, col) {
 	}
 
 	return temp;
-}
-
-function setRow(dir, row, temp) {
-	if (dir === DIR_LEFT) {
-		gameBoard[row][0] = temp[3];
-		gameBoard[row][1] = temp[2];
-		gameBoard[row][2] = temp[1];
-		gameBoard[row][3] = temp[0];
-	} else if (dir === DIR_RIGHT) {
-		gameBoard[row][0] = temp[0];
-		gameBoard[row][1] = temp[1];
-		gameBoard[row][2] = temp[2];
-		gameBoard[row][3] = temp[3];
-	}
 }
 
 function setCol(dir, col, temp) {
@@ -122,207 +132,16 @@ function flushLine(line) {
 }
 
 function solveLine(line) {
-	var move = false;
-
 	for (var index = 3; index >= 1; index--) {
 		try {
-			if (temp[index] === temp[index - 1]) {
-				temp[index] *= 2;
-				temp[index - 1] = 0;
+			if (line[index] === line[index - 1]) {
+				line[index] *= 2;
+				line[index - 1] = 0;
 				// TODO: react to move
-				move = true;
 			}
 			line = flushLine(line);
 		} catch (ex) {}
-	};
-}
-
-console.log("Before");
-gameBoard.forEach(function(item) { console.log(item) });
-
-
-function moveDown() {
-	var move = false;
-
-	for (var col = 0; col < 4; col++) {
-		var temp = [0,0,0,0];
-		var rowToPlaceNewNumber = 3;
-
-		// Move all the numbers down
-		for (var row = 3, _index = 3; row >= 0; row--)
-			if (gameBoard[row][col] !== 0)
-				temp[_index--] = gameBoard[row][col];
-
-		// Start from the 2nd to last row, then move up
-		for (var row = 2; row >= 0; row--) {
-			try {
-				if (temp[row] !== 0) {
-					// If the numbers match
-					if (temp[row] === temp[rowToPlaceNewNumber]) {
-						temp[rowToPlaceNewNumber] *= 2;
-						temp[row] = 0;
-						rowToPlaceNewNumber -= 1;		// Move up 2 rows
-						row--;
-						// TODO: react to move
-						move = true;
-					}
-					else if (temp[rowToPlaceNewNumber] === 0) {
-						temp[rowToPlaceNewNumber] = temp[row];
-						temp[row] = 0;
-						rowToPlaceNewNumber -= 1;		// Move up 1 row
-						move = true;
-					} else {
-						rowToPlaceNewNumber -= 1;
-					}
-				}
-			} catch (ex) {}
-		}
-
-		gameBoard[0][col] = temp[0];
-		gameBoard[1][col] = temp[1];
-		gameBoard[2][col] = temp[2];
-		gameBoard[3][col] = temp[3];
 	}
 
-	return move;
-}
-
-function moveRight(){
-	var move = false;
-
-	for (var col = 0; col < 4; col++) {
-		var temp = [0,0,0,0];
-		var rowToPlaceNewNumber = 3;
-
-		// Move all the numbers down
-		for (var row = 3, _index = 3; row >= 0; row--)
-			if (gameBoard[row][col] !== 0)
-				temp[_index--] = gameBoard[row][col];
-
-		// Start from the 2nd to last row, then move up
-		for (var row = 2; row >= 0; row--) {
-			try {
-				if (temp[row] !== 0) {
-					// If the numbers match
-					if (temp[row] === temp[rowToPlaceNewNumber]) {
-						temp[rowToPlaceNewNumber] *= 2;
-						temp[row] = 0;
-						rowToPlaceNewNumber -= 2;		// Move up 2 rows
-						// TODO: react to move
-						move = true;
-					}
-					else if (temp[rowToPlaceNewNumber] === 0) {
-						temp[rowToPlaceNewNumber] = temp[row];
-						temp[row] = 0;
-						rowToPlaceNewNumber -= 1;		// Move up 1 row
-						move = true;
-					} else {
-						rowToPlaceNewNumber -= 1;
-					}
-				}
-			} catch (ex) {}
-		}
-
-		gameBoard[0][col] = temp[0];
-		gameBoard[1][col] = temp[1];
-		gameBoard[2][col] = temp[2];
-		gameBoard[3][col] = temp[3];
-	}
-
-	return move;
-}
-
-function moveUp() {
-	var move = false;
-
-	for (var col = 0; col < 4; col++) {
-		var temp = [0,0,0,0];
-		var rowToPlaceNewNumber = 3;
-
-		// Move the current colum into temp in reverse order
-		for (var row = 0, _index = 3; row < 4; row++)
-			if (gameBoard[row][col] !== 0)
-				temp[_index--] = gameBoard[row][col];
-
-		// Start from the 2nd to last row, then move up
-		for (var row = 2; row >= 0; row--) {
-			try {
-				if (temp[row] !== 0) {
-					// If the numbers match
-					if (temp[row] === temp[rowToPlaceNewNumber]) {
-						temp[rowToPlaceNewNumber] *= 2;
-						temp[row] = 0;
-						rowToPlaceNewNumber -= 2;		// Move up 2 rows
-						// TODO: react to move
-						move = true;
-					}
-					else if (temp[rowToPlaceNewNumber] === 0) {
-						temp[rowToPlaceNewNumber] = temp[row];
-						temp[row] = 0;
-						rowToPlaceNewNumber -= 1;		// Move up 1 row
-						move = true;
-					} else {
-						rowToPlaceNewNumber -= 1;
-					}
-				}
-			} catch (ex) {}
-		}
-
-		gameBoard[0][col] = temp[0];
-		gameBoard[1][col] = temp[1];
-		gameBoard[2][col] = temp[2];
-		gameBoard[3][col] = temp[3];
-	}
-
-	return move;
-}
-
-function moveLeft(){
-	var move = false;
-
-	for (var col = 0; col < 4; col++) {
-		var temp = [0,0,0,0];
-		var rowToPlaceNewNumber = 3;
-
-		// Move all the numbers down
-		for (var row = 3, _index = 3; row >= 0; row--)
-			if (gameBoard[row][col] !== 0)
-				temp[_index--] = gameBoard[row][col];
-
-		// Start from the 2nd to last row, then move up
-		for (var row = 2; row >= 0; row--) {
-			try {
-				if (temp[row] !== 0) {
-					// If the numbers match
-					if (temp[row] === temp[rowToPlaceNewNumber]) {
-						temp[rowToPlaceNewNumber] *= 2;
-						temp[row] = 0;
-						rowToPlaceNewNumber -= 2;		// Move up 2 rows
-						// TODO: react to move
-						move = true;
-					}
-					else if (temp[rowToPlaceNewNumber] === 0) {
-						temp[rowToPlaceNewNumber] = temp[row];
-						temp[row] = 0;
-						rowToPlaceNewNumber -= 1;		// Move up 1 row
-						move = true;
-					} else {
-						rowToPlaceNewNumber -= 1;
-					}
-				}
-			} catch (ex) {}
-		}
-
-		gameBoard[0][col] = temp[0];
-		gameBoard[1][col] = temp[1];
-		gameBoard[2][col] = temp[2];
-		gameBoard[3][col] = temp[3];
-	}
-
-	return move;
-}
-
-while(moveDown()){
-	console.log("After")
-	gameBoard.forEach(function(item) { console.log(item) });;
+	return line;
 }
